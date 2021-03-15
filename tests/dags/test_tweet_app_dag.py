ff22c4476d5483c03b7116871f8a2b6d176e994f
Jeferson Machado Santos <jefersonmsantos@outlook.com>
2021-03-15 08:49:41 -0300

    tests_folder

---
+++ tests/dags/test_tweet_app_dag.py
@@ -0,0 +1,22 @@
+import pytest
+from dags.tweet_app_dag import consult_tweet_api, read_txt_save_json, process_json_to_df_to_sql, remove_files
+
+#test for consult_tweet_api
+
+
+
+
+
+#test for read_txt_save_json
+
+
+
+
+
+#test for process_json_to_df_to_sql
+
+
+
+
+
+#test for remove_files
